54c66b5c1f14b62e4ad443671e243d85230c88a7
Nathan Vale <hi@nathanvale.com>
2019-01-21 13:12:53 +1100

    chore(wallaby): fix broken tests and consolidate configs

--- wallaby.form-foundations.js
+++
@@ -1,6 +0,0 @@
-// TODO: move to wallaby.config.js
-const wallabyBase = require('./wallaby.base');
-module.exports = wallabyBase(
-  ['packages/*/src/**'],
-  ['packages/*/src/**/*.spec.{ts,tsx}'],
-);
