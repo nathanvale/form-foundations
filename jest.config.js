d58b130b3b4e819e2197016c981ee65496ca614c
Nathan Vale <hi@nathanvale.com>
2019-01-22 16:43:30 +1100

    ci: implement codecov on ci

--- jest.config.js
+++ jest.config.js
@@ -5,21 +5,4 @@ module.exports = {
   ...base,
   setupTestFrameworkScriptFile: './jest.setup.js',
   projects: ['<rootDir>', '<rootDir>/packages/*/jest.config.js'],
-  coverageReporters: ['lcov', 'text-summary'],
-  coverageDirectory: '<rootDir>/coverage/',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js}',
-    '!dist/**',
-    '!src/**/index.{ts,tsx,js}',
-    '!babel.config.js',
-    '!jest.config.js',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 48.84,
-      functions: 37.3,
-      lines: 49.42,
-      statements: 45.83,
-    },
-  },
 };
--- jest.settings.js
+++ jest.settings.js
@@ -3,6 +3,15 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx|js)$': 'babel-jest',
   },
+  coverageReporters: ['json'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js}',
+    '!dist/**',
+    '!src/**/index.{ts,tsx,js}',
+    '!babel.config.js',
+    '!jest.config.js',
+  ],
+  coverageDirectory: '<rootDir>/coverage/',
   testMatch: ['<rootDir>/src/**/?(*.)spec.ts?(x)'],
   setupTestFrameworkScriptFile: '<rootDir>../../jest.setup.js',
   moduleFileExtensions: ['js', 'json', 'node', 'ts', 'tsx'],
